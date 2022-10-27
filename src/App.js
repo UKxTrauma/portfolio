684c1ddf6bc68db43512bfaabbadb85940861bb0
Brett Garrett <b.t.garrett@aol.co.uk>
2022-10-27 01:20:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='header'>
+        <header>
+          MY PORTFOLIO
+        </header>
+      </div>
+      <div id='nav'>
+        <div>
+          <p>APIs</p>
+          <a href='https://smoothieapi.netlify.app' target='_blank' className='error'>Smooth(ie)-API</a>
+          <a href='https://cats4lyf-desk3.netlify.app/' target='_blank'>Cats4Lyf</a>
+          <a href='https://fetchapicn.netlify.app/' target='_blank'>Anime-API</a>
+        </div>
+        <div>
+          <p>Websites</p>
+          <a href='https://ukxtrauma.github.io/VampireLegion/' target='_blank'>Vampire-Legion</a>
+          <a href='https://ukxtrauma.github.io/CSSAnimationTest/' target='_blank'>CSS-Animation-Test</a>
+          <a href='https://innovateproject.herokuapp.com/' target='_blank'>Mystery-Site</a>
+        </div>
+        <div>
+          <p>Apps</p>
+          <a href='https://todolistcn.netlify.app/' target='_blank'>Todo-List</a>
+          <a href='https://ukxtrauma.github.io/dicegame/' target='_blank'>Dice-Game</a>
+          <a href='https://ukxtrauma.github.io/drumkit/' target='_blank'>Drumkit</a>
+          <a href='https://ukxtrauma.github.io/Keycode/' target='_blank'>Keycode</a>
+          <a href='https://calculatorcn.netlify.app/' target='_blank'>Calculator</a>
+        </div>
+        <p id='thanks'>Thanks for browsing!</p>
+      </div>
+      <div id='contact'>
+        <p>Contact me on LinkedIn: <a href='https://www.linkedin.com/in/brett-garrett-2a2155238/' target='_blank'>Brett Garrett</a></p>
+      </div>
     </div>
   );
 }
